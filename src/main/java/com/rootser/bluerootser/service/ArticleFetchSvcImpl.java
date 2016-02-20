0a545af3e04048171b5b349aac339fae19459f14
john <jhancock1975@gmail.com>
2016-02-20 13:04:15 -0500

    pass url pattern as parameter

--- src/main/java/com/rootser/bluerootser/service/ArticleFetchSvcImpl.java
+++ src/main/java/com/rootser/bluerootser/service/ArticleFetchSvcImpl.java
@@ -17,12 +17,12 @@ import com.rootser.bluerootser.model.Article;
 public class ArticleFetchSvcImpl implements ArticleFetchSvc {
 	private static final Logger logger = LoggerFactory.getLogger(ArticleFetchSvcImpl.class);
 	@Override
-	public List<Article> getArticles(int numArticles, String baseURl) {
+	public List<Article> getArticles(int numArticles, String baseURl, String urlPattern) {
 		List<Article> result = new ArrayList<Article>();
 		try {
 			String ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36";
 			Document doc = Jsoup.connect(baseURl).userAgent(ua).get();
-			Elements links = doc.select("a[href*=/news/]");
+			Elements links = doc.select("a[href*=" + urlPattern + "]");
 			int count = 1;
 			for (Element link: links){
 				logger.debug(link.attr("href"));
