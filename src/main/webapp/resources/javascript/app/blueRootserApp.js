29b63f1c7a58dc7fe6beff1fc24716bb71fef761
john <jhancock1975@gmail.com>
2016-02-23 21:24:49 -0500

    capture text highlight event

--- src/main/webapp/resources/javascript/app/blueRootserApp.js
+++ src/main/webapp/resources/javascript/app/blueRootserApp.js
@@ -1,66 +1,78 @@
 /**
  * 
  */
-// create the module and name it blueRootserApp
+//create the module and name it blueRootserApp
 var blueRootserApp = angular.module('blueRootserApp', [ 'ui.bootstrap',
-		'ngRoute','ngSanitize', 'onSelect']);
+                                                        'ngRoute','ngSanitize', 'onSelect']);
 
 //configure our routes
 blueRootserApp.config(function($routeProvider) {
-    $routeProvider
+	$routeProvider
 
-        // route for the home page
-        .when('/', {
-            templateUrl : '/home',
-            controller  : 'mainController'
-        })
+	// route for the home page
+	.when('/', {
+		templateUrl : '/home',
+		controller  : 'mainController'
+	})
 
-        // route for the login page
-        .when('/login', {        	
-            templateUrl : '/loginView',
-            controller  : 'loginController'
-        })
-        
-        
-        // route for the contact page
-        .when('/myArticles', {
-            templateUrl : '/myArticles',
-            controller  : 'myArticlesController'
-        });
+	// route for the login page
+	.when('/login', {        	
+		templateUrl : '/loginView',
+		controller  : 'loginController'
+	})
+
+
+	// route for the contact page
+	.when('/myArticles', {
+		templateUrl : '/myArticles',
+		controller  : 'myArticlesController'
+	});
 
 
 });
 
-// create the controller and inject Angular's $scope
+//create the controller and inject Angular's $scope
 blueRootserApp.controller('mainController', function($scope, $http) {
-    // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
-    var data = {};
-    $http.defaults.headers.post['X-CSRF-TOKEN'] = csrfTokenVal;
-    result = $http.post("/articles", data).success(function(data, status, headers, config)	{
-    	console.log('success');
-    	$scope.artcileHtml =  data;
-    }).error(function(data, status, headers, config){
-    	console.log('error');
-    	console.log("data = " + data);
-    	console.log("scope.message = " + scope.message)
-    });
-    
-    var vm = this;
-    vm.handler = handler;
- 
-    function handler(selection) {
-      setTimeout(function() {
-        selection.removeHighlight();
-      }, 2000);
-    };
-    
+	// create a message to display in our view
+	$scope.message = 'Everyone come and see how good I look!';
+	$scope.onselect ="";
+
+	var data = {};
+	$http.defaults.headers.post['X-CSRF-TOKEN'] = csrfTokenVal;
+	result = $http.post("/articles", data).success(function(data, status, headers, config)	{
+		console.log('success');
+		$scope.artcileHtml =  data;
+	}).error(function(data, status, headers, config){
+		console.log('error');
+		console.log("data = " + data);
+		console.log("scope.message = " + scope.message)
+	});
+
+	$scope.onselect = function(){
+		console.log("on select");
+	}
+
+});
+
+blueRootserApp.directive("highlight", function() {
+	return function(scope, element, attrs) {
+		element.on('mouseup', function(event) {
+			console.log("mouse up");
+			var text = "";
+			if (window.getSelection) {
+				text = window.getSelection().toString();
+			} else if (document.selection && document.selection.type != "Control") {
+				text = document.selection.createRange().text;
+			}
+			console.log(text);
+		})
+	}
 });
 
 blueRootserApp.controller('loginController', function($scope) {
-    $scope.message = 'Look! I am the login page.';
+	$scope.message = 'Look! I am the login page.';
 });
 
 blueRootserApp.controller('myArticlesController', function($scope) {
-    $scope.message = 'Contact us! JK. This is just a demo.';
+	$scope.message = 'Contact us! JK. This is just a demo.';
 });
